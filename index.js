9d93dc3f8860da8c85b402044dfcdb3b6db77b39
Ana Zitzow <anasempai89@gmail.com>
2022-05-04 19:03:09 -0700

    commit tree c03ec7040bce3d759dc482da8303b2d1892aac0f

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', () => {
+  // console.log('The DOM has loaded')
+  const text = document.getElementById('text');
+  console.log(text);
+  text.textContent = 'This is really cool!';
+
+});
